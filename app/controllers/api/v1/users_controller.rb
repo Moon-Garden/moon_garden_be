88e2d32638f6a75c6285380fe6a2bcd6ab773616
Bryce Wein <bryce.wein@gmail.com>
2022-07-27 17:57:05 -0400

    Test: user creation test

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -18,7 +18,7 @@ class Api::V1::UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      render json: UserSerializer.new(@user), status: :created, location: @user
+      render json: UserSerializer.new(@user), status: :created
     else
       render json: @user.errors, status: :unprocessable_entity
     end
---
+++ spec/requests/api/v1/user_request_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+
+RSpec.describe 'Users API' do
+  describe 'happy path' do
+    it 'creates a user' do
+      user_params = {
+        name: 'Saba',
+        email: 'saba@saba.com',
+        image: 'thisisanimage'
+      }
+      headers = { 'CONTENT_TYPE' => 'application/json' }
+
+      post '/api/v1/users', headers: headers, params: JSON.generate(user: user_params)
+
+      expect(response).to be_successful
+
+      parsed_body = JSON.parse(response.body, symbolize_names: true)
+
+      expect(parsed_body).to have_key(:data)
+      expect(parsed_body[:data]).to be_a Hash
+
+      user = parsed_body[:data]
+
+      expect(user).to have_key :id
+      expect(user[:id]).to be_a String
+      expect(user).to have_key :type
+      expect(user[:type]).to eq('user')
+
+      expect(user).to have_key :attributes
+
+      expect(user[:attributes]).to have_key :name
+      expect(user[:attributes][:name]).to be_a String
+      expect(user[:attributes][:name]).to eq(user_params[:name])
+
+      expect(user[:attributes]).to have_key :email
+      expect(user[:attributes][:email]).to be_a String
+      expect(user[:attributes][:email]).to eq(user_params[:email])
+
+      expect(user[:attributes]).to have_key :image
+      expect(user[:attributes][:image]).to be_a String
+      expect(user[:attributes][:image]).to eq(user_params[:image])
+
+      expect(user).to_not have_key :created_at
+      expect(user).to_not have_key :updated_at
+    end
+  end
+end
