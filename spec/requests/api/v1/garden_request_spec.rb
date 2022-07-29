5ac6eeb56806c6813c90a170630b4ce24cec4bda
Andrew King <dev.andrewking@gmail.com>
2022-07-29 15:47:53 -0500

    add garden request spec

---
+++ spec/requests/api/v1/garden_request_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+
+RSpec.describe 'Users API' do
+  describe 'happy path' do
+    it 'creates a garden' do
+      user = create(:user)
+      garden_json = 
+        {
+          "user_id": user.id,
+          "name": "value1",
+          "notes": "value2",
+          "cardinal_direction": 1
+        }
+      headers = { 'CONTENT_TYPE' => 'application/json' }
+
+      post '/api/v1/gardens', headers: headers, params: JSON.generate(garden_json)
+
+      expect(response).to be_successful
+
+      parsed_body = JSON.parse(response.body, symbolize_names: true)
+
+      expect(parsed_body).to have_key(:data)
+      expect(parsed_body[:data]).to be_a Hash
+
+      garden = parsed_body[:data]
+
+      expect(garden).to have_key :id
+      expect(garden[:id]).to be_a String
+      expect(garden).to have_key :type
+      expect(garden[:type]).to eq('garden')
+
+      expect(garden).to have_key :attributes
+
+      expect(garden[:attributes]).to have_key :name
+      expect(garden[:attributes][:name]).to be_a String
+
+      expect(garden[:attributes]).to have_key :cardinal_direction
+      expect(garden[:attributes][:cardinal_direction]).to be_a String
+
+      expect(garden[:attributes]).to have_key :notes
+      expect(garden[:attributes][:notes]).to be_a String
+
+      expect(garden).to_not have_key :created_at
+      expect(garden).to_not have_key :updated_at
+    end
+  end
+end
